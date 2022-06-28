4dd9f8d2f1ca55949cbb9c9f8d7aeb54dbbcca05
jilvins <juris.kucinskis@gmail.com>
2022-06-28 13:10:41 +0300

    Created module and db schema for blog posts

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Author: Juris Kucinskis
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Mans_Blog', __DIR__);
